9a9aef9dd65f24ddd76a4ed5f23dac746f4cbe89
Josue Dos Santos Marques <josue.santos@verdesmares.com.br>
2022-11-07 10:34:21 -0300

    test

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 import streamlit as st
-import os
 
-@st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
-_ = installff()
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
+
+st.balloons()
+with webdriver.Chrome(options=options, service_log_path='selenium.log') as driver:
+    url = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+    driver.get(url)
+    xpath = '//*[@class="ui-mainview-block eventpath-wrapper"]'
+    # Wait for the element to be rendered:
+    element = WebDriverWait(driver, 10).until(lambda x: x.find_elements(by=By.XPATH, value=xpath))
+    name = element[0].get_property('attributes')[0]['name']
+    st.write(name)
+
+    driver.get('http://example.com')
+    st.write(driver.page_source)
 
 from partials.navbar import streamlit_menu
 from services.download_img import download_img
 from services.screenshots import screenshots
 from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
 
 
 def main():
@@ -22,13 +40,6 @@ def main():
 
     st.header('Serviços Python')
     st.caption('by. Josué Santos')
-
-    opts = FirefoxOptions()
-    opts.add_argument("--headless")
-    browser = webdriver.Firefox(options=opts)
-
-    browser.get('http://example.com')
-    st.write(browser.page_source)
 
     selected = streamlit_menu()
 
