9308f1d83b3b9c7310511d229dfdd274e33138a6
Josue Dos Santos Marques <josue.santos@verdesmares.com.br>
2022-11-16 13:29:17 -0300

    test

--- services/screenshots.py
+++ services/screenshots.py
@@ -7,6 +7,13 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from io import StringIO
+
+import pyperclip
+
 
 options = Options()
 options.add_argument("--headless")
@@ -22,43 +29,61 @@ def screenshots():
     st.markdown('#### &#x25A3; Download de Imagens de Screenshots')
     st.write('Copie os links. Screenshot realizado em 800px de largura e 1280px de altura.')
     
-    if st.button('Colar da área de transferência'):
-        global data_file
-        data_file = pd.read_clipboard(names=['url'])
-        st.dataframe(data_file)
+    st.write(pyperclip.paste())
 
-    if st.button('Os dados estão corretos?'):
-        st.warning('Aguarde o Botão Download aparecer.')
+    copy_button = Button(label="Get Clipboard Data")
+    copy_button.js_on_event("button_click", CustomJS(code="""
+        navigator.clipboard.readText().then(text => document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: text})))
+        """))
+    result = streamlit_bokeh_events(
+        copy_button,
+        events="GET_TEXT",
+        key="get_text",
+        refresh_on_update=False,
+        override_height=75,
+        debounce_time=0)
+
+    if result:
+        if "GET_TEXT" in result:
+            df = pd.read_csv(StringIO(result.get("GET_TEXT")))
+            st.table(df)
+    # if st.button('Colar da área de transferência'):
+    #     global data_file
+    #     data_file = pd.read_clipboard(names=['url'])
+    #     st.dataframe(data_file)
+
+    # if st.button('Os dados estão corretos?'):
+    #     st.warning('Aguarde o Botão Download aparecer.')
         
-        my_bar = st.progress(0)
-        size_file = data_file.size
-        column1, column2 = st.columns(2)
-        with zipfile.ZipFile("hello.zip", mode="w") as archive:
-            for index, row in data_file.iterrows():
-                my_bar.progress((index + 1) / size_file)
-                filename = ''
-                try:
-                    with webdriver.Chrome(options=options) as browser:
-                        browser.get(row['url'])
+    #     my_bar = st.progress(0)
+    #     size_file = data_file.size
+    #     column1, column2 = st.columns(2)
+    #     with zipfile.ZipFile("hello.zip", mode="w") as archive:
+    #         for index, row in data_file.iterrows():
+    #             my_bar.progress((index + 1) / size_file)
+    #             filename = ''
+    #             try:
+    #                 with webdriver.Chrome(options=options) as browser:
+    #                     browser.get(row['url'])
 
-                        title = browser.title
-                        filename = title + ".jpg"
+    #                     title = browser.title
+    #                     filename = title + ".jpg"
 
-                        browser.save_screenshot(filename)
+    #                     browser.save_screenshot(filename)
 
-                        archive.write(filename)
-                        os.remove(filename)
-                except:
-                    with column2:
-                        st.write("NOT FOUND - " + row['url'])
+    #                     archive.write(filename)
+    #                     os.remove(filename)
+    #             except:
+    #                 with column2:
+    #                     st.write("NOT FOUND - " + row['url'])
     
-        with column1:
-            with open("hello.zip", "rb") as file:
-                st.balloons()
-                st.download_button(
-                    label="Download ZIP",
-                    data=file,
-                    file_name='ScreenshotZIP-pyService.zip',
-                    mime="application/zip",
-                )
+    #     with column1:
+    #         with open("hello.zip", "rb") as file:
+    #             st.balloons()
+    #             st.download_button(
+    #                 label="Download ZIP",
+    #                 data=file,
+    #                 file_name='ScreenshotZIP-pyService.zip',
+    #                 mime="application/zip",
+    #             )
     
